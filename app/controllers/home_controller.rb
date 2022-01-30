15be7e100988fe7f3b6cb2c0a0f1025d61219db7
Kobato13 <johncarlomartinez13@gmail.com>
2022-01-30 16:50:11 +0800

    Added header and footer and installed font awesome

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,8 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      @name = current_user.first_name + ' ' + current_user.last_name   
+      @name = current_user.first_name + ' ' + current_user.last_name
+      @type = current_user.user_type
     else 
       @name = "Stranger"
     end
