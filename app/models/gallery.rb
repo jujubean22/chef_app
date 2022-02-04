7261fdaf41e712844ad90d1071b3381b08c7b57f
jujubean22 <jmevillanueva22@gmail.com>
2022-02-05 01:37:47 +0800

    added gallery to profile, added gallery_controller, updated chef_controller and profile view,  added gallery model active storage

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,3 +1,4 @@
 class Gallery < ApplicationRecord
   belongs_to :chef
+  has_one_attached :image
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'clients/index'
   get 'chefs/index'
-  devise_for :users
+  devise_for :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'cities/:state', to: 'application#cities'
 
   # scope '/admins' do
   #   resources :users
@@ -11,7 +12,9 @@ Rails.application.routes.draw do
 
   # resources :users do
   #   resources :clients
-  #   resources :chefs
+    resources :chefs do
+      resources :galleries
+    end
   # end
   
   devise_scope :users do
