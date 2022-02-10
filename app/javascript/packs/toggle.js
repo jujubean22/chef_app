af5e85fcf20fd2b1dbf5bac3d6ae4b68b51727dc
tknlibunao <thea.kaylee.libunao@eee.upd.edu.ph>
2022-02-10 23:57:48 +0800

    modified toggle.js, added toggle button classname

--- app/javascript/packs/toggle.js
+++ app/javascript/packs/toggle.js
@@ -1,21 +1,31 @@
-function toggle() {
-  var content = document.getElementById('toggle-div');
-  // console.log('I clicked toggle!')
+function toggle(e) {
+  if (e.target.id == 'toggle-service')
+    id = 'service-form';
+  else if (e.target.id == 'toggle-gallery')
+    id = 'gallery-form';
+
+  var content = document.getElementById(`${id}`);
   if (content.style.display == 'none') {
-    // console.log('None now, toggle to block')
     content.style.display = 'block';
-    document.getElementById('toggle-btn').value = 'Hide service form';
+    document.getElementById(`${e.target.id}`).value = id == 'service-form' ? 'Hide service form' : 'Hide gallery form';
+    document.getElementById(`${e.target.id}`).className = 'btn-small btn-hide';
   } else {
-    // console.log('Block now, toggle to none')
     content.style.display = 'none';
-    document.getElementById('toggle-btn').value = 'Add new service';
+    document.getElementById(`${e.target.id}`).value = id == 'service-form' ? 'Add new service' : 'Add new photo';
+    document.getElementById(`${e.target.id}`).className = 'btn-small btn-show';
   }
 }
 
 document.addEventListener('turbolinks:load', () => {
-  const clickButton = document.getElementById('toggle-btn');
-  clickButton.addEventListener('click', (event) => {
-    toggle()
+  const serviceButton = document.getElementById('toggle-service');
+  const galleryButton = document.getElementById('toggle-gallery');
+  
+  serviceButton.addEventListener('click', (e) => {
+    toggle(e)
+  });
+  
+  galleryButton.addEventListener('click', (e) => {
+    toggle(e)
   });
 });
 
